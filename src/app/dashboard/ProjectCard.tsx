735ba09dea62d16065c42de4962ef101bb23caf2
S1lva <diogomcsilva5@gmail.com>
2026-02-08 01:12:59 +0000

    style: changed UI style and structor of the page

--- src/app/dashboard/ProjectCard.tsx
+++ src/app/dashboard/ProjectCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Project } from "../../types/Project"
 import Link from "next/link";
-
+import "./ProjectCard.css"
 type ProjectCardProps = {
   project: Project;
   handleDeleteProject: (id: string) => void
@@ -9,13 +9,23 @@ type ProjectCardProps = {
 
 const ProjectCard = ({project, handleDeleteProject}: ProjectCardProps) => {
   return (
-    <div>
-        <Link href={`/dashboard/projects/${project.id}`}>
-          <p>{project.name}</p>
-        </Link>
-          <button onClick={() => handleDeleteProject(project.id)}>Delete Project</button>
+    <div className="project-card-container">
+      <Link href={`/dashboard/projects/${project.id}`} className="project-card">
+        <img src="/images/folder.png" alt="folder" />
+        <p>{project.name}</p>
+      </Link>
+
+      <button
+        className="delete-btn"
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          handleDeleteProject(project.id);
+        }}
+      >
+        ×
+      </button>
     </div>
   )
 }
-
 export default ProjectCard
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,60 +1,92 @@
-"use client"
-import React, { useEffect, useState } from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { getStorageItem } from "../../utils/storage"
-import { createProject, listProjects, deleteProject } from "../../services/project.service"
-import { deleteTasksByProject } from "../../services/task.service"
-import { Project } from "../../types/Project"
-import ProjectCard from "./ProjectCard"
-import Stats from './Stats';
+import { getStorageItem } from "../../utils/storage";
+import {
+  createProject,
+  listProjects,
+  deleteProject,
+} from "../../services/project.service";
+import { deleteTasksByProject } from "../../services/task.service";
+import { Project } from "../../types/Project";
+import ProjectCard from "./ProjectCard";
+import Stats from "./Stats";
+import styles from "./page.module.css";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import SearchBar from "./SearchBar";
 
 const page = () => {
   const router = useRouter();
-  const [name, setName] = useState<string>("")
-  const [projects, setProjects] = useState<Project[]>([])
+  const [name, setName] = useState<string>("");
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [search, setSearch] = useState("");
+  const filteredProjects = projects.filter((project) =>
+    project.name.includes(search.toLowerCase()),
+  );
 
   useEffect(() => {
-      let isSaved = getStorageItem("email")
-      if(!isSaved){
-          router.push("/");
-      }
-  }, [router])
+    let isSaved = getStorageItem("email");
+    if (!isSaved) {
+      router.push("/");
+    }
+  }, [router]);
 
   useEffect(() => {
     setProjects(listProjects());
   }, []);
 
-  function handleDeleteProject(projectId: string){
-    deleteTasksByProject(projectId)
-    deleteProject(projectId)
-    setProjects(listProjects())
-    console.log(projectId)
+  function handleDeleteProject(projectId: string) {
+    deleteTasksByProject(projectId);
+    deleteProject(projectId);
+    setProjects(listProjects());
+    console.log(projectId);
   }
 
   return (
-    <div>
-      <form onSubmit={e => {
-        e.preventDefault();
-        createProject(name)
-        setProjects(listProjects())
-      }}>
-        <input type="text" value={name} onChange={e => setName(e.target.value)} />
-        <input type="submit" value="Add Project" />
-      </form>
-
+    <div className={styles.page}>
       <Stats />
+      <div className={styles.header}>
+        <div className={styles.searchContainer}>
+          <SearchBar search={search} setSearch={setSearch} />
+        </div>
 
-      {
-      projects.map(project => (
-        <ProjectCard 
-          key={project.id} 
-          project={project} 
-          handleDeleteProject={handleDeleteProject}
-        />
-      ))
-      }
+        <form
+          className={styles.formAddProjects}
+          onSubmit={(e) => {
+            e.preventDefault();
+            createProject(name);
+            setProjects(listProjects());
+          }}
+        >
+          <Input
+            type="text"
+            Icon="deployed_code"
+            placeholder="Add Project"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Button type="submit" value="Add Project" />
+        </form>
+      </div>
+
+      
+        {filteredProjects.length !== 0 ? (
+          <div className={styles.projectsGrid}>
+{            filteredProjects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              project={project}
+              handleDeleteProject={handleDeleteProject}
+            />
+          ))}
+          </div>
+        ) : (
+          <p className={styles.nothingP}>Cant find any Projects...</p>
+        )}
+      
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
